c00bbb7c9fa11d714eb68c81ac8363bfece0da94
Dang Hai An <an@izberg-marketplace.com>
2017-03-09 23:19:00 +0100

    fix: improve bookmark item layout

--- src/bookmarks/components/BookmarkItem.js
+++ src/bookmarks/components/BookmarkItem.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import { Row, Col } from 'reactstrap';
 import moment from 'moment';
-import firebase from 'firebase';
 import { browserHistory } from 'react-router';
 
 import './BookmarkItem.css';
 
-const recipesRef = firebase.database().ref('recipes');
 const BookmarkItem = React.createClass({
   render() {
     const {
@@ -13,20 +12,23 @@ const BookmarkItem = React.createClass({
     } = this.props;
 
     return (
-      <div className="bookmark-item" onClick={() => {
-        browserHistory.push(`/bookmark/${bookmarkId}`);
-      }}>
-        <div className="bookmark-main-info">
-          <h4>{bookmark.get('meal')}</h4>
-          <p>{moment(bookmark.get('date')).fromNow()}</p>
-          <p>{bookmark.get('name')}</p>
-        </div>
-        <div className="bookmark-right-info">
-          <p>{bookmark.get('duration')} min cook</p>
-        </div>
-        <div className="icon">
-          <i className="fa fa-chevron-right" aria-hidden="true"></i>
-        </div>
+      <div className="bookmark-item">
+        <Row className="bookmark-item" onClick={() => {
+          browserHistory.push(`/bookmark/${bookmarkId}`);
+        }}>
+          <Col xs="6" className="bookmark-date">
+            {moment(bookmark.get('date')).fromNow()}
+          </Col>
+          <Col xs="6" className="bookmark-duration">
+            <p>{bookmark.get('duration')} min cook</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={{ size: 11, offset: 1 }}>
+            <h4>{bookmark.get('meal')}</h4>
+            <p className="bookmark-note">{bookmark.get('note')}</p>
+          </Col>
+        </Row>
       </div>
     );
   }
