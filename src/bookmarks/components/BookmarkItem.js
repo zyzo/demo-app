23290f4dfdee7cfad609a1045f68d30181dd5f1e
Dang Hai An <an@izberg-marketplace.com>
2017-03-09 23:23:54 +0100

    fix: more bookmark layout improv

--- src/bookmarks/components/BookmarkItem.js
+++ src/bookmarks/components/BookmarkItem.js
@@ -1,34 +1,22 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
 import moment from 'moment';
-import { browserHistory } from 'react-router';
 
 import './BookmarkItem.css';
 
 const BookmarkItem = React.createClass({
   render() {
     const {
-      bookmark, bookmarkId
+      bookmark
     } = this.props;
 
     return (
       <div className="bookmark-item">
-        <Row className="bookmark-item" onClick={() => {
-          browserHistory.push(`/bookmark/${bookmarkId}`);
-        }}>
-          <Col xs="6" className="bookmark-date">
-            {moment(bookmark.get('date')).fromNow()}
-          </Col>
-          <Col xs="6" className="bookmark-duration">
-            <p>{bookmark.get('duration')} min cook</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={{ size: 11, offset: 1 }}>
-            <h4>{bookmark.get('meal')}</h4>
-            <p className="bookmark-note">{bookmark.get('note')}</p>
-          </Col>
-        </Row>
+        <div className="bookmark-date">
+          {moment(bookmark.get('date')).fromNow()}
+        </div>
+        <p className="bookmark-duration">{bookmark.get('duration')} min cook</p>
+        <h4>{bookmark.get('meal')}</h4>
+        <p className="bookmark-note">{bookmark.get('note')}</p>
       </div>
     );
   }
