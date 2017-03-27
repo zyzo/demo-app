3d5134c6117c183638b1a05daca322cd9ac99f40
Dang Hai An <an@izberg-marketplace.com>
2017-03-28 00:11:36 +0200

    fix: style

--- src/ingredients/components/IngredientItem.js
+++ src/ingredients/components/IngredientItem.js
@@ -11,9 +11,11 @@ const IngredientItem = React.createClass({
       <div className={
         `ingredient-item${selected ? ' selected' : ''}`}
         onClick={onClick}>
+        <div style={{ float: 'right', color: '#999' }}>{ingredient.getIn(['recipe', 'bookmark', 'date'])}</div>
         <input type="checkbox" checked={!!selected}/>
         <span className="ingredient-item-name">{ingredient.get('name')}</span>
         <div style={{ color: '#999' }}>{ingredient.getIn(['recipe', 'name'])}</div>
+        <div className="clearfix"></div>
       </div>
     );
   }
